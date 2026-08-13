6d078e264d117868107e0789c6bc536be1aedbe4
Allan <Allan.gabriel587@gmail.com>
2026-08-12 21:25:48 -0300

    Adicionando projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { Component } from '../../node_modules/@angular/compiler/types/compiler';
+import { Menu } from './component/menu/menu';
 
 export const routes: Routes = [];
---
+++ src/app/component/atleta/atleta.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atleta',
+  imports: [],
+  templateUrl: './atleta.html',
+  styleUrl: './atleta.css',
+})
+export class Atleta {}
