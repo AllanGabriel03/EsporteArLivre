a500b227ac8a2df3f8177b3ad4d14fa3a7be94fe
Allan <allan.gabriel587@gmail.com>
2026-09-02 21:28:54 -0300

    Criando a função de calcular IMC

--- src/app/component/atleta-lista-component/atleta-lista-component.ts
+++ src/app/component/atleta-lista-component/atleta-lista-component.ts
@@ -65,8 +65,25 @@ export class AtletaListaComponent {
     this.router.navigate(['/cadastroatleta', idAtleta])
   }
 
-  calcIdade(dataNascimento: string){
-    return this.http.calcularIdade(dataNascimento)
+  calcIdade(data_nascimento: string){
+    return this.http.calcularIdade(data_nascimento)
   }
   
+  calcImc(peso: number, altura: number){
+    let imc = (peso / (altura * altura))
+    let situacao = ''
+
+    if(imc < 18.5){
+     situacao = "Magreza" 
+    }else if(imc >= 18.5 && imc <= 24.9){
+     situacao = "Peso Normal"
+    }else if(imc >= 25 && imc <= 29.9){
+      situacao = "Sobrepeso"
+    }else if(imc >= 30 && imc <= 39.9){
+      situacao = "Obesidade"
+    }else{situacao = "Obesidade grave"}
+
+    return situacao
+  }
+
 }//FIM COMPONENT AtletaListaComponent
